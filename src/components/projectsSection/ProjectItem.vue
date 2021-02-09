4c41dc089b38d9fdac007d106630c8fbe8fc8534
maciejziemichod <maciej.ziemichod@gmail.com>
2021-02-09 23:17:39 +0100

    feat: about and projects responsiveness

--- src/components/projectsSection/ProjectItem.vue
+++ src/components/projectsSection/ProjectItem.vue
@@ -1,26 +1,33 @@
 <template>
-  <article :class="orientation">
-    <div class="about-project">
-      <h2>
-        {{ data.title }}
-      </h2>
-      <p>
-        {{ data.description }}
-      </p>
-      <div>
-        <LabelItem v-for="label in data.labels" :key="label" :text="label" />
-      </div>
-      <div class="icons-container">
-        <IconItem icon="github" :link="data.githubLink" />
-        <IconItem icon="link" :link="data.pageLink" />
-      </div>
-    </div>
-    <figure>
-      <a :href="data.pageLink" target="_blank" rel="noopener noreferrer">
-        <img :src="require(`../../assets/${data.image}`)" alt="project image" />
-      </a>
-    </figure>
-  </article>
+    <article :class="orientation">
+        <div class="about-project">
+            <h2>
+                {{ data.title }}
+            </h2>
+            <p>
+                {{ data.description }}
+            </p>
+            <div class="labels">
+                <LabelItem
+                    v-for="label in data.labels"
+                    :key="label"
+                    :text="label"
+                />
+            </div>
+            <div class="icons-container">
+                <IconItem icon="github" :link="data.githubLink" />
+                <IconItem icon="link" :link="data.pageLink" />
+            </div>
+        </div>
+        <figure>
+            <a :href="data.pageLink" target="_blank" rel="noopener noreferrer">
+                <img
+                    :src="require(`../../assets/${data.image}`)"
+                    alt="project image"
+                />
+            </a>
+        </figure>
+    </article>
 </template>
 
 <script>
@@ -28,105 +35,138 @@ import LabelItem from "../utilities/LabelItem.vue";
 import IconItem from "../utilities/IconItem.vue";
 
 export default {
-  name: "ProjectItem",
-  props: {
-    data: {
-      required: true,
-      validator: function ({
-        title,
-        description,
-        labels,
-        image,
-        pageLink,
-        githubLink,
-      }) {
-        if (!title || typeof title !== "string") return false;
-        if (!description || typeof description !== "string") return false;
-        if (!labels || typeof labels !== "object") return false;
-        if (!image || typeof image !== "string") return false;
-        if (!pageLink || typeof pageLink !== "string") return false;
-        if (!githubLink || typeof githubLink !== "string") return false;
-        return true;
-      },
+    name: "ProjectItem",
+    props: {
+        data: {
+            required: true,
+            validator: function ({
+                title,
+                description,
+                labels,
+                image,
+                pageLink,
+                githubLink,
+            }) {
+                if (!title || typeof title !== "string") return false;
+                if (!description || typeof description !== "string")
+                    return false;
+                if (!labels || typeof labels !== "object") return false;
+                if (!image || typeof image !== "string") return false;
+                if (!pageLink || typeof pageLink !== "string") return false;
+                if (!githubLink || typeof githubLink !== "string") return false;
+                return true;
+            },
+        },
+        orientation: {
+            type: String,
+            default: "left",
+            validator: function (value) {
+                return ["left", "right"].indexOf(value) !== -1;
+            },
+        },
     },
-    orientation: {
-      type: String,
-      default: "left",
-      validator: function (value) {
-        return ["left", "right"].indexOf(value) !== -1;
-      },
+    components: {
+        LabelItem,
+        IconItem,
     },
-  },
-  components: {
-    LabelItem,
-    IconItem,
-  },
 };
 </script>
 
 <style scoped>
 h2 {
-  font-size: 28px;
-  font-weight: normal;
+    font-size: 28px;
+    font-weight: normal;
 }
 
 article {
-  display: flex;
-  justify-content: space-between;
+    display: flex;
+    justify-content: space-between;
 }
 article:not(:first-of-type) {
-  padding-top: 150px;
+    padding-top: 150px;
 }
 article.left {
-  flex-direction: row;
-  text-align: left;
+    flex-direction: row;
+    text-align: left;
 }
 article.left p {
-  padding-right: 40px;
+    padding-right: 40px;
 }
 article.right {
-  flex-direction: row-reverse;
-  text-align: right;
+    flex-direction: row-reverse;
+    text-align: right;
 }
 article.right p {
-  padding-left: 40px;
+    padding-left: 40px;
 }
 article > * {
-  width: 50%;
+    width: 50%;
 }
 
 h2 {
-  padding-bottom: 40px;
+    padding-bottom: 40px;
 }
 
 img {
-  box-shadow: 0 10px 30px -15px var(--dark);
-  width: 100%;
-  object-fit: contain;
-  /* height: 340px; */
-  transition: 0.3s box-shadow;
+    box-shadow: 0 10px 30px -15px var(--dark);
+    width: 100%;
+    object-fit: contain;
+    /* height: 340px; */
+    transition: 0.3s box-shadow;
 }
 img:hover {
-  box-shadow: 0 20px 40px -15px var(--dark);
+    box-shadow: 0 20px 40px -15px var(--dark);
 }
 
 p {
-  font-size: 18px;
+    font-size: 18px;
 
-  padding-bottom: 20px;
+    padding-bottom: 20px;
 }
 
 .icons-container {
-  padding-top: 20px;
-  font-size: 20px;
+    padding-top: 20px;
+    font-size: 20px;
 }
 .icons-container > *:first-child {
-  padding-right: 10px;
+    padding-right: 10px;
 }
 
 .about-project {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+}
+
+@media (max-width: 768px) {
+    article.left,
+    article.right {
+        flex-direction: column-reverse;
+        text-align: left;
+    }
+    article > * {
+        width: initial;
+    }
+    article.right p,
+    article.left p {
+        padding: 0 0 1rem;
+    }
+    h2 {
+        padding: 1rem 0;
+        text-align: center;
+    }
+    .icons-container {
+        text-align: center;
+        font-size: 1.5rem;
+    }
+    .icons-container > a {
+        padding: 0 1.5rem;
+    }
+    article:not(:first-of-type) {
+        padding-top: 100px;
+    }
+    .labels {
+        text-align: center;
+    }
 }
 </style>
--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,24 +1,24 @@
 <template>
-  <div>
-    <SecondNavbar />
-    <main class="container">
-      <section>
-        <ProjectItem
-          v-for="(project, index) in projects"
-          :key="project.title"
-          :data="project"
-          :orientation="index % 2 === 0 ? 'left' : 'right'"
-        />
-      </section>
-      <div class="return">
-        <router-link to="/">
-          <ButtonItem size="sm"> Return </ButtonItem>
-        </router-link>
-      </div>
-    </main>
-    <ScrollTop />
-    <FooterItem />
-  </div>
+    <div>
+        <SecondNavbar />
+        <main class="container">
+            <section>
+                <ProjectItem
+                    v-for="(project, index) in projects"
+                    :key="project.title"
+                    :data="project"
+                    :orientation="index % 2 === 0 ? 'left' : 'right'"
+                />
+            </section>
+            <div class="return">
+                <router-link to="/">
+                    <ButtonItem size="sm"> Return </ButtonItem>
+                </router-link>
+            </div>
+        </main>
+        <ScrollTop />
+        <FooterItem />
+    </div>
 </template>
 
 <script>
@@ -29,71 +29,77 @@ import ButtonItem from "@/components/utilities/ButtonItem.vue";
 import FooterItem from "@/components/FooterItem.vue";
 
 export default {
-  name: "Projects",
-  components: {
-    ProjectItem,
-    ButtonItem,
-    SecondNavbar,
-    ScrollTop,
-    FooterItem,
-  },
-  data() {
-    return {
-      projects: [
-        {
-          title: "Web Clipboard",
-          description:
-            "Web app made for personal use. It helps in my current job, when I have repetitive topics that day. Easy to use clipboard manager. Made with Vue 3.",
-          labels: ["Vue 3", "Local Storage", "Vuex"],
-          githubLink: "https://github.com/maciejziemichod/web-clipboard",
-          pageLink: "https://maciejziemichod.github.io/web-clipboard/",
-          image: "web_clipboard-min.png",
-        },
-        {
-          title: "Pomodoro Clock",
-          description:
-            "React pomodoro clock. It helps managing time, splitting it between work and breaks.",
-          labels: ["React", "Redux", "React Hooks"],
-          githubLink: "https://github.com/maciejziemichod/Pomodoro-Clock",
-          pageLink: "https://codepen.io/maciejziemichod/full/gOPZYMy",
-          image: "pomodoro_clock-min.png",
-        },
-        {
-          title: "Random Quote Generator",
-          description: "Nothing else to say, stay inspired.",
-          labels: ["React", "API", "Bootstrap"],
-          githubLink:
-            "https://github.com/maciejziemichod/Random-Quote-Generator",
-          pageLink: "https://codepen.io/maciejziemichod/full/YzyJmeG",
-          image: "random_quote_generator-min.png",
-        },
-        {
-          title: "File Metadata Microservice",
-          description:
-            "Microservice that analyses uploaded file and responses with JSON containing file's name and size.",
-          labels: ["Express", "BEM", "Sass"],
-          githubLink: "https://github.com/maciejziemichod/File-Metadata",
-          pageLink: "https://sugary-lyrical-buttercup.glitch.me/",
-          image: "file_metadata_microservice-min.jpeg",
-        },
-        {
-          title: "Drum machine",
-          description: "Online drums, it works both with mouse and keyboard.",
-          labels: ["React", "React Hooks", "Bootstrap"],
-          githubLink: "https://github.com/maciejziemichod/Drum-Machine",
-          pageLink: "https://codepen.io/maciejziemichod/full/KKdOpyM",
-          image: "drum_machine-min.png",
-        },
-      ],
-    };
-  },
+    name: "Projects",
+    components: {
+        ProjectItem,
+        ButtonItem,
+        SecondNavbar,
+        ScrollTop,
+        FooterItem,
+    },
+    data() {
+        return {
+            projects: [
+                {
+                    title: "Web Clipboard",
+                    description:
+                        "Web app made for personal use. It helps in my current job, when I have repetitive topics that day. Easy to use clipboard manager. Made with Vue 3.",
+                    labels: ["Vue 3", "Local Storage", "Vuex"],
+                    githubLink:
+                        "https://github.com/maciejziemichod/web-clipboard",
+                    pageLink:
+                        "https://maciejziemichod.github.io/web-clipboard/",
+                    image: "web_clipboard-min.png",
+                },
+                {
+                    title: "Pomodoro Clock",
+                    description:
+                        "React pomodoro clock. It helps managing time, splitting it between work and breaks.",
+                    labels: ["React", "Redux", "React\u00A0Hooks"],
+                    githubLink:
+                        "https://github.com/maciejziemichod/Pomodoro-Clock",
+                    pageLink: "https://codepen.io/maciejziemichod/full/gOPZYMy",
+                    image: "pomodoro_clock-min.png",
+                },
+                {
+                    title: "Random Quote Generator",
+                    description: "Nothing else to say, stay inspired.",
+                    labels: ["React", "API", "Bootstrap"],
+                    githubLink:
+                        "https://github.com/maciejziemichod/Random-Quote-Generator",
+                    pageLink: "https://codepen.io/maciejziemichod/full/YzyJmeG",
+                    image: "random_quote_generator-min.png",
+                },
+                {
+                    title: "File Metadata Microservice",
+                    description:
+                        "Microservice that analyses uploaded file and responses with JSON containing file's name and size.",
+                    labels: ["Express", "BEM", "Sass"],
+                    githubLink:
+                        "https://github.com/maciejziemichod/File-Metadata",
+                    pageLink: "https://sugary-lyrical-buttercup.glitch.me/",
+                    image: "file_metadata_microservice-min.jpeg",
+                },
+                {
+                    title: "Drum machine",
+                    description:
+                        "Online drums, it works both with mouse and keyboard.",
+                    labels: ["React", "React\u00A0Hooks", "Bootstrap"],
+                    githubLink:
+                        "https://github.com/maciejziemichod/Drum-Machine",
+                    pageLink: "https://codepen.io/maciejziemichod/full/KKdOpyM",
+                    image: "drum_machine-min.png",
+                },
+            ],
+        };
+    },
 };
 </script>
 
 <style scoped>
 .return {
-  width: fit-content;
-  margin: 0 auto;
-  padding-bottom: 50px;
+    width: fit-content;
+    margin: 0 auto;
+    padding-bottom: 50px;
 }
 </style>
